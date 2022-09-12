b12388039295dadd26768de2cec84ed027bb1a99
CiaranJRobinson <ciaranjayrobinson@gmail.com>
2022-09-12 08:16:26 -0400

    changes?

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 // const SERVER_PORT = 4004;
 const port = process.env.PORT || 4004;
-// const path = require('path')
+const path = require('path')
 const app = express()
 const cors = require('cors')
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/index.html', function(req,res){
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+app.get('/index.js', function(req,res){
+    res.sendFile(path.join(__dirname, '/public/index.js'));
+});
+app.get('/index.css', function(req,res){
+    res.sendFile(path.join(__dirname, '/public/index.css'));
+});
 
 
 const {submitBtn, getResults, getDogs, getTheDogList} = require('./controller');
